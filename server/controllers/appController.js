03cd1680d75db423fe45ec1abc8a53d21b366944
reyanzaman <reyanzaman.rz@gmail.com>
2023-03-05 20:36:07 +0600

    Temporary check

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,5 +1,7 @@
 import UserModel from '../model/User.model.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import ENV from '../config.js';
 
 /** POST http://localhost:8080/api/register
  * @param : {
@@ -69,44 +71,36 @@ export async function register(req, res) {
  * "password" : "admin123"
  * }
  */
-export async function login(req, res){
-    
+export async function login(req, res) {
     const { username, password } = req.body;
 
     try {
-        
-        UserModel.findOne({ username })
-            .then(user => {
-                bcrypt.compare(password, user.password)
-                    .then(passwordCheck => {
+        const user = await UserModel.findOne({ username });
 
-                        if(!passwordCheck) return res.status(400).send({ error: "Password is missing"});
+        if (!user) {
+            return res.status(404).send({ error: "Username not found" });
+        }
 
-                        // create jwt token
-                        const token = jwt.sign({
-                                        userId: user._id,
-                                        username : user.username
-                                    }, ENV.JWT_SECRET , { expiresIn : "24h"});
+        const passwordCheck = await bcrypt.compare(password, user.password);
 
-                        return res.status(200).send({
-                            msg: "Login Successful...!",
-                            username: user.username,
-                            token
-                        });                                    
+        if (!passwordCheck) {
+            return res.status(400).send({ error: "Password does not match" });
+        }
 
-                    })
-                    .catch(error =>{
-                        return res.status(400).send({ error: "Passwords does not Match"})
-                    })
-            })
-            .catch( error => {
-                return res.status(404).send({ error : "Username not Found"});
-            })
+        const token = jwt.sign(
+            { userId: user._id, username: user.username },
+            ENV.JWT_SECRET,
+            { expiresIn: "24h" }
+        );
 
+        return res.status(200).send({
+            msg: "Login Successful",
+            username: user.username,
+            token
+        });
     } catch (error) {
-        return res.status(500).send({ error});
+        return res.status(500).send({ error: error.message });
     }
-
 }
 
 /** GET: http://localhost:8080/api/user/example123 */
