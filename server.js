f96e2067709fe74f4c94f373e89957a658609e5c
reyanzaman <reyanzaman.rz@gmail.com>
2023-03-26 20:47:45 +0600

    cors update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json({limit: '2mb'}));
 app.use(express.urlencoded({limit: '2mb', extended: true}));
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:3000', 'https://question-based-learning.onrender.com', 'https://question-based-learning-api.onrender.com'], credentials: true }))
 app.use(morgan('tiny'));
 app.disable('x-powered-by');
 
